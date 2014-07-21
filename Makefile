494261583eb0ff0ed7964582b76fbe6baf4404cc
Jan Bölsche <regular.gonzales@googlemail.com>
2014-07-21 16:56:50 +0200

    now using highland

--- Makefile
+++ Makefile
@@ -2,11 +2,7 @@ BIN:=$(shell npm bin)
 
 all: build/episode.html
 
-build/rl.html: resources.yaml
-	mkdir -p build
-	$(BIN)/realist < resources.yaml > build/rl.html
-
-build/episode.html: build/rl.html episode.md layout.html
+build/episode.html: episode.md layout.html
 	./make.js > build/episode.html
 
 clean:
--- make.js
+++ make.js
@@ -1,35 +1,48 @@
 #!/usr/bin/env node
 
+var _ = require('highland');
 var ejs = require('ejs');
 var fs = require('fs');
 var marked = require('marked');
 var yaml = require('js-yaml');
 var inspect = require('util').inspect;
 
-var segments = [];
-var currDoc = null;
-// we receive meta data and text docs interleaved, starting with meta data
-yaml.safeLoadAll(fs.readFileSync('episode.md', 'utf8'), function(doc) {
-    if (currDoc !== null) {
-        currDoc.text = doc;
-        segments.push(currDoc);
-        currDoc = null;
-    } else {
-        currDoc = doc;
-    }
-});
-// console.error(inspect(yamlDocs, {depth: null}));
+var compileReadingList = require('insertmode-reading-list');
 
-function render(segment) {
+function readYAMLDocs(filename) {
+    var data = fs.readFileSync(filename, 'utf8');
+    var docs = [];
+    yaml.safeLoadAll(data, function(doc) {
+        docs.push(doc);
+    });
+    return docs;
+}
+// we receive meta data and text docs interleaved, starting with meta data
+var segments = _(readYAMLDocs('episode.md')).batch(2).map(function(l) {
+    l[0].text = l[1];
+    return l[0];
+});
+
+/* segments.toArray(function(x) {
+    console.error(inspect(x, {depth: null}));
+}); */
+
+function render(segment, cb) {
     var templateName = segment.template || 'section.ejs';
     var templatePath = 'templates/' + templateName;
     segment.text = marked(segment.text || '');
     var template = fs.readFileSync(templatePath, 'utf8');
-    return ejs.render(template, segment);
+    cb(null, ejs.render(template, segment));
 } 
 
-var content = segments.map(render).join('');
-var layout = fs.readFileSync('layout.html', 'utf8');
-var page = ejs.render(layout, {content: content});
-console.log(page);
+// concat rendered segments and put into layout
+render = _.wrapCallback(render);
+var content = segments.flatMap(render).collect();
 
+var page = content.map(function(x) {
+    var content = x.join('');
+    var layout = fs.readFileSync('layout.html', 'utf8');
+    return ejs.render(layout, {content: content});
+}); 
+
+page.pipe(process.stdout);
